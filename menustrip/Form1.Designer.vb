f9bdb8ff0fb490e00453655403e70c9948eaf161
ANG1110 <angxq-wm19@student.tarc.edu.my>
2020-08-13 22:45:10 +0800

    commit by ANG1110

--- menustrip/Form1.Designer.vb
+++ menustrip/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Form2
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -25,17 +25,11 @@ Partial Class Form1
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.mnuHome = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuHomeExit = New System.Windows.Forms.ToolStripMenuItem()
-        Me.mnuView = New System.Windows.Forms.ToolStripMenuItem()
-        Me.mnuViewOrder = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuUser = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuUserLogin = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuUserStaff = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuMovie = New System.Windows.Forms.ToolStripMenuItem()
-        Me.mnuMovieMenu = New System.Windows.Forms.ToolStripMenuItem()
-        Me.mnuMovieOrder = New System.Windows.Forms.ToolStripMenuItem()
-        Me.mnuMoviePromotion = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuPayment = New System.Windows.Forms.ToolStripMenuItem()
-        Me.mnuPaymentTotalPayment = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuPaymentReceipt = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuPaymentRefund = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuUpdate = New System.Windows.Forms.ToolStripMenuItem()
@@ -43,12 +37,14 @@ Partial Class Form1
         Me.mnuDisplay = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuHelpAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BookingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PromotionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuHome, Me.mnuView, Me.mnuUser, Me.mnuMovie, Me.mnuPayment, Me.mnuUpdate, Me.mnuDelete, Me.mnuDisplay, Me.mnuHelp})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuHome, Me.mnuUser, Me.mnuMovie, Me.BookingToolStripMenuItem, Me.PromotionToolStripMenuItem, Me.mnuPayment, Me.mnuUpdate, Me.mnuDelete, Me.mnuDisplay, Me.mnuHelp})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
@@ -68,19 +64,6 @@ Partial Class Form1
         Me.mnuHomeExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.F4), System.Windows.Forms.Keys)
         Me.mnuHomeExit.Size = New System.Drawing.Size(180, 22)
         Me.mnuHomeExit.Text = "E&xit"
-        '
-        'mnuView
-        '
-        Me.mnuView.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuViewOrder})
-        Me.mnuView.Name = "mnuView"
-        Me.mnuView.Size = New System.Drawing.Size(44, 20)
-        Me.mnuView.Text = "&View"
-        '
-        'mnuViewOrder
-        '
-        Me.mnuViewOrder.Name = "mnuViewOrder"
-        Me.mnuViewOrder.Size = New System.Drawing.Size(180, 22)
-        Me.mnuViewOrder.Text = "O&rder"
         '
         'mnuUser
         '
@@ -103,41 +86,16 @@ Partial Class Form1
         '
         'mnuMovie
         '
-        Me.mnuMovie.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuMovieMenu, Me.mnuMovieOrder, Me.mnuMoviePromotion})
         Me.mnuMovie.Name = "mnuMovie"
         Me.mnuMovie.Size = New System.Drawing.Size(52, 20)
         Me.mnuMovie.Text = "&Movie"
         '
-        'mnuMovieMenu
-        '
-        Me.mnuMovieMenu.Name = "mnuMovieMenu"
-        Me.mnuMovieMenu.Size = New System.Drawing.Size(180, 22)
-        Me.mnuMovieMenu.Text = "Mo&vie menu"
-        '
-        'mnuMovieOrder
-        '
-        Me.mnuMovieOrder.Name = "mnuMovieOrder"
-        Me.mnuMovieOrder.Size = New System.Drawing.Size(180, 22)
-        Me.mnuMovieOrder.Text = "Or&der"
-        '
-        'mnuMoviePromotion
-        '
-        Me.mnuMoviePromotion.Name = "mnuMoviePromotion"
-        Me.mnuMoviePromotion.Size = New System.Drawing.Size(180, 22)
-        Me.mnuMoviePromotion.Text = "&Promotion"
-        '
         'mnuPayment
         '
-        Me.mnuPayment.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuPaymentTotalPayment, Me.mnuPaymentReceipt, Me.mnuPaymentRefund})
+        Me.mnuPayment.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuPaymentReceipt, Me.mnuPaymentRefund})
         Me.mnuPayment.Name = "mnuPayment"
         Me.mnuPayment.Size = New System.Drawing.Size(66, 20)
         Me.mnuPayment.Text = "&Payment"
-        '
-        'mnuPaymentTotalPayment
-        '
-        Me.mnuPaymentTotalPayment.Name = "mnuPaymentTotalPayment"
-        Me.mnuPaymentTotalPayment.Size = New System.Drawing.Size(180, 22)
-        Me.mnuPaymentTotalPayment.Text = "Total pa&yment"
         '
         'mnuPaymentReceipt
         '
@@ -183,14 +141,27 @@ Partial Class Form1
         Me.mnuHelpAbout.Size = New System.Drawing.Size(180, 22)
         Me.mnuHelpAbout.Text = "&About"
         '
-        'Form1
+        'BookingToolStripMenuItem
+        '
+        Me.BookingToolStripMenuItem.Name = "BookingToolStripMenuItem"
+        Me.BookingToolStripMenuItem.Size = New System.Drawing.Size(97, 20)
+        Me.BookingToolStripMenuItem.Text = "&Booking Ticket"
+        '
+        'PromotionToolStripMenuItem
+        '
+        Me.PromotionToolStripMenuItem.Name = "PromotionToolStripMenuItem"
+        Me.PromotionToolStripMenuItem.Size = New System.Drawing.Size(76, 20)
+        Me.PromotionToolStripMenuItem.Text = "&Promotion"
+        '
+        'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.MenuStrip1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form2"
+        Me.Text = "MenuStrip"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
@@ -201,17 +172,11 @@ Partial Class Form1
     Friend WithEvents MenuStrip1 As Windows.Forms.MenuStrip
     Friend WithEvents mnuHome As ToolStripMenuItem
     Friend WithEvents mnuHomeExit As ToolStripMenuItem
-    Friend WithEvents mnuView As ToolStripMenuItem
-    Friend WithEvents mnuViewOrder As ToolStripMenuItem
     Friend WithEvents mnuUser As ToolStripMenuItem
     Friend WithEvents mnuUserLogin As ToolStripMenuItem
     Friend WithEvents mnuUserStaff As ToolStripMenuItem
     Friend WithEvents mnuMovie As ToolStripMenuItem
-    Friend WithEvents mnuMovieMenu As ToolStripMenuItem
-    Friend WithEvents mnuMovieOrder As ToolStripMenuItem
-    Friend WithEvents mnuMoviePromotion As ToolStripMenuItem
     Friend WithEvents mnuPayment As ToolStripMenuItem
-    Friend WithEvents mnuPaymentTotalPayment As ToolStripMenuItem
     Friend WithEvents mnuPaymentReceipt As ToolStripMenuItem
     Friend WithEvents mnuPaymentRefund As ToolStripMenuItem
     Friend WithEvents mnuUpdate As ToolStripMenuItem
@@ -219,4 +184,6 @@ Partial Class Form1
     Friend WithEvents mnuDisplay As ToolStripMenuItem
     Friend WithEvents mnuHelp As ToolStripMenuItem
     Friend WithEvents mnuHelpAbout As ToolStripMenuItem
+    Friend WithEvents BookingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PromotionToolStripMenuItem As ToolStripMenuItem
 End Class
--- menustrip/Form1.vb
+++ menustrip/Form1.vb
@@ -1,3 +1,3 @@
-﻿Public Class Form1
+﻿Public Class Form2
 
 End Class
